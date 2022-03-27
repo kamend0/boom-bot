d217aafb267eee83031056767017e3ba6eb665ad
Kamend0 <mendoza.kollin@gmail.com>
2022-03-26 20:12:35 -0700

    Initial commit of functioning boom-bot.

---
+++ main.py
@@ -0,0 +1,49 @@
+import discord
+from discord.ext import commands
+from discord import FFmpegPCMAudio
+import environ
+import time
+
+env = environ.Env()
+environ.Env.read_env()
+
+intents = discord.Intents.default()
+intents.members = True
+
+client = commands.Bot(command_prefix = '!!', intents = intents)
+
+
+@client.event
+async def on_ready():
+    print("\nBot launched successfully!\n")
+
+@client.event
+async def on_voice_state_update(member, before, after):
+    if "boom-bot" in member.roles:
+        return
+
+    if not before.channel and after.channel:
+        source = FFmpegPCMAudio('comedy-sound.mp3')
+        voice.play(source)
+    # if before.channel and not after.channel:
+    #     print(f'{member.name} has left the VC')
+
+@client.command(pass_context = True)
+async def join(ctx):
+    if (ctx.author.voice):
+        channel = ctx.message.author.voice.channel
+        global voice
+        voice = await channel.connect()
+    else:
+        await ctx.send(ctx.author.name + ": You have to join a voice channel first.")
+
+@client.command(pass_context = True)
+async def leave(ctx):
+    if (ctx.voice_client):
+        await ctx.guild.voice_client.disconnect()
+        await ctx.send("Left the voice channel.")
+    else:
+        await ctx.send(ctx.author.name + ": I'm not in a voice channel.")
+
+
+client.run(env("DISCORD_TOKEN"))
