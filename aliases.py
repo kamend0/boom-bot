70cbfbfdef8485c770da8d9fd28fa6b2b550e71a
Kamend0 <mendoza.kollin@gmail.com>
2022-04-06 20:33:59 -0700

    Prepare for deployment via Heroku

---
+++ aliases.py
@@ -0,0 +1,19 @@
+aliases = {
+    'koopakev' : 'Kevin, who loves women,',
+    'FlyinFunk' : 'Sammy-Wammy with that wet wet 3000, gushing gawker-walker throat goat,',
+    'kamendo' : 'My father',
+    'tonto' : 'Peter',
+    'Shiba42x' : 'Josh\'s stupid ass',
+    'Chief Ponce' : 'Andrew',
+    'zjepson' : 'Zac',
+    'willloveslakers' : 'Will',
+    'Pun_Dorry' : 'Loot Goblin',
+    'EstebanCeja' : 'Esteban',
+    'art' : 'Sarah Jay enjoyer',
+    'BigStinky' : 'Steve, of all fucking people',
+    'Crymacho' : 'Alex, who hates women,',
+    'WTFSomebodyActually' : 'Harbey',
+    'Chon Wang' : 'Kuya boy',
+    'Chankles' : 'Charles',
+    'Scuzi' : 'Scooter'
+}
